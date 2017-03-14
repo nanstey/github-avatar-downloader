0e3bae89d876bf3125c6f0373161b54890869d77
Noel Anstey <anstey.ng@gmail.com>
2017-03-14 02:48:43 +0000

    fixed syntax with eslint

--- download_avatars.js
+++ download_avatars.js
@@ -15,7 +15,7 @@
 // ================================== //
 
 // require `dotenv`, `request`, the Node `fs` (filesystem) module, & `mkdirp` for creating directory
-require('dotenv').config()
+require('dotenv').config();
 var request = require('request');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
@@ -36,7 +36,7 @@ var USR_AGENT = "Avatar Downloader";
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // Create Request URL and Request Options
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var options = {
     'url': requestURL,
     'headers': {
@@ -46,7 +46,9 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   // GET request
   request.get(options, function(err, response, body) {
-    if (err) throw err; // Boo! Error
+    if (err){
+      throw err;
+    }
     // Log request and respnse status
     console.log('Requesting /repos/' + repoOwner + '/' + repoName + '/contributors ...');
     console.log('Response Status Code:', response.statusCode, '\n');
@@ -55,29 +57,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
     if (response.statusCode === 200){
       // Response OK!
       var data = JSON.parse(body);
-      cb(data); // Callback with data
+      // Callback with data
+      cb(data);
     } else {
       // Response NOT OK :(
       console.log('Something went wrong ... check that your arguments are valid.');
     }
   });
-}
-
-// downloadContributorImages()
-//    Traverses contributor JSON data
-//    Downloads each image into avatars/ directory
-
-function downloadContributorImages(data) {
-  // Create avatars directory if it doesn't exist
-  mkdirp('avatars/', function (err) {
-    if (err) console.error(err)
-    else console.log('Created avatars/ directory ...')
-  });
-
-  for (var i in data){
-    // Callback => downloadImageByURL()
-    downloadImageByURL( data[i]['avatar_url'], 'avatars/' + data[i]['login'] );
-  }
 }
 
 // downloadImageByURL()
@@ -89,18 +75,39 @@ function downloadImageByURL(url, filePath) {
 
   request.get(url)
         .on('error', function (err) {
-          throw err; // Boo! Error
+          throw err;
         })
         .on('response', function (response) {
           // Log response code + filepath & get image type
           console.log('Response Status Code: ' + response.statusCode + ' ' + filePath );
           type = response.headers['content-type'].replace('/image\//', '');
         })
-        .pipe(fs.createWriteStream(filePath + type)) // Save the image
+        // Save the image
+        .pipe(fs.createWriteStream(filePath + type))
         .on('finish', function() {
-          // Log
+          // Log when download complete
           console.log('Download complete: ' + filePath);
         });
+}
+
+// downloadContributorImages()
+//    Traverses contributor JSON data
+//    Downloads each image into avatars/ directory
+
+function downloadContributorImages(data) {
+  // Create avatars directory if it doesn't exist
+  mkdirp('avatars/', function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Created avatars/ directory ...');
+    }
+  });
+
+  for (var i in data){
+    // Callback => downloadImageByURL()
+    downloadImageByURL( data[i]['avatar_url'], 'avatars/' + data[i]['login'] );
+  }
 }
 
 // ================================== //
